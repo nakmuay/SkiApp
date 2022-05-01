8b55e690b62aea95356b463a1c608605b6c368bb
MartinFagerlind <mfagerlind@hotmail.com>
2022-05-01 23:35:32 +0200

    Fix computer used for calculating children's skis.

--- SkiiApp.Services/ChildrensSkiiLengthComputer.cs
+++ SkiiApp.Services/ChildrensSkiiLengthComputer.cs
@@ -8,7 +8,14 @@
     {
         public Either<string, SkiiLengthResult> Compute(int height, SkiiType type)
         {
-            return Either<string, SkiiLengthResult>.Right(new SkiiLengthResult(height + 10));
+            // Assume minimum skii length is 1m.
+            var skiiLength = Math.Max(height, 100);
+            if (skiiLength > 192)
+            {
+                return Either<string, SkiiLengthResult>.Left("Are you sure you are 4 years or younger?");
+            }
+
+            return Either<string, SkiiLengthResult>.Right(new SkiiLengthResult(skiiLength));
         }
     }
 }
